7a26a9cb36566f7ca673301b6db5024831f7898c
Mah Tej Rella <mahtejrella@gmail.com>
2022-05-03 18:08:20 -0400

    Pay Employee and Hire Worker for Manager screen implemented

--- components/manager/HireWorker.js
+++ components/manager/HireWorker.js
@@ -1,5 +1,4 @@
 import {React, useState, useEffect} from 'react'
-import { useRouter } from 'next/dist/client/router'
 import axios from 'axios';
 
 export default function HireWorker() {
@@ -7,9 +6,10 @@ export default function HireWorker() {
   const [bankList, setBankList] = useState([])
   const [employeeList, setEmployeeList] = useState([])
 
-  const [selectedBank, setSelectedBank] = useState()
-  const [selectedEmployee, setSelectedEmployee] = useState()
+  const [selectedBank, setSelectedBank] = useState("BA_South")
+  const [selectedEmployee, setSelectedEmployee] = useState("arwhite6")
 
+  const [salary, setSalary] = useState()
 
   useEffect(() => {
     async function getData(){
@@ -30,6 +30,13 @@ export default function HireWorker() {
     getData2()
   }, [])
 
+  const onSubmit=async(e)=>{
+    e.preventDefault();
+    const payload = {selectedEmployee, selectedBank, salary}
+    console.log(payload);
+    let data = await axios.post('/api/workFor', payload);
+  }
+
   return (
     <>
       <div className="hidden sm:block" aria-hidden="true">
@@ -41,7 +48,7 @@ export default function HireWorker() {
       <div className="mt-10 sm:mt-0">
         <div className="md:grid md:grid-cols-3 md:gap-6">
           <div className="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
+            <form onSubmit={onSubmit}>
               <div className="shadow overflow-hidden sm:rounded-md">
                 <div className="px-4 py-5 bg-white sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
@@ -75,13 +82,29 @@ export default function HireWorker() {
                       <select
                         id="country"
                         value={selectedEmployee}
-                        onChange={(e) => setSelectedBank(e.target.value)}
+                        onChange={(e) => setSelectedEmployee(e.target.value)}
                         className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                       >
                       {employeeList.map((x, i) =>
                         <option key={x}>{x.perID}</option>
                       )}
                       </select>
+                    </div>
+
+                    <div className="col-span-6 sm:col-span-4">
+                      <label
+                        htmlFor="email-address"
+                        className="block text-sm font-medium text-gray-700"
+                      >
+                        $New salary
+                      </label>
+                      <input
+                        type="text"
+                        name="email-address"
+                        value={salary}
+                        onChange={(e) => setSalary(e.target.value)}
+                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow sm:text-sm border-gray-300 rounded-md"
+                      />
                     </div>
                   </div>
                 </div>
--- components/manager/PayEmployee.js
+++ components/manager/PayEmployee.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import axios from 'axios';
 
 export default function PayEmployee() {
+
+  const onSubmit=async(e)=>{
+    e.preventDefault();
+    let data = await axios.post('/api/pay');
+  }
+
   return (
       <>
         
@@ -10,15 +17,16 @@ export default function PayEmployee() {
           </div>
         </div>
 
-        <div>
-            <button
-                type="submit"
-                className="inline-flex justify-center py-2 px-4 border border-transparent shadow text-sm font-medium rounded-md text-white bg-black hover:bg-yellow-500 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-               Pay Employee 
-            </button>
-        </div>
-
+        <form onSubmit={onSubmit}>
+          <div>
+              <button
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow text-sm font-medium rounded-md text-white bg-black hover:bg-yellow-500 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Pay Employee 
+              </button>
+          </div>
+        </form>
       </>
   )
 }
